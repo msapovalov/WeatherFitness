bf31da7a0d00a91aa279c2f575573ce99f2c51b0
harshitkumar0603 <harshit.kumar@ucdconnect.ie>
2017-11-04 13:05:30 +0000

    test app

--- app/src/main/java/com/ucd/user/weatherfitness/MainActivity.java
+++ app/src/main/java/com/ucd/user/weatherfitness/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 
+        //Test comment added on github
+
     }
 }
