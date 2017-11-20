931ae62f2dcbcb6374b0ba14315c9ed7b8008ff5
itbstudent <b00066331@student.itb.ie>
2017-11-20 01:21:31 +0000

    Added tabbed history activity

--- app/src/main/java/com/ucd/user/weatherfitness/MainActivity.java
+++ app/src/main/java/com/ucd/user/weatherfitness/MainActivity.java
@@ -1,53 +1,141 @@
 package com.ucd.user.weatherfitness;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import com.ucd.user.weatherfitness.model.FetchWeatherTask;
 
+
 public class MainActivity extends AppCompatActivity {
+    TextView tvAddress;
+    AppLocationService appLocationService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView score_id = (TextView)findViewById(R.id.score_ID);
+        tvAddress = (TextView) findViewById(R.id.tvAddress);
+        appLocationService = new AppLocationService(
+                MainActivity.this);
+
+        TextView score_id = findViewById(R.id.score_ID);
         FetchWeatherTask weatherTask = new FetchWeatherTask(score_id);
         weatherTask.execute("7778677");
 
-        Button btn = (Button) findViewById(R.id.button1);
-        btn.setOnClickListener(new View.OnClickListener(){
+        Button btn = findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v) {
+            public void onClick(View v) {
                 Intent newIntent = new Intent(MainActivity.this, MapsActivity.class);
-                MainActivity.this.startActivity(newIntent);}
+                MainActivity.this.startActivity(newIntent);
+            }
         });
 
-        Button btn1 = (Button) findViewById(R.id.button2);
-        btn1.setOnClickListener(new View.OnClickListener(){
+        Button btn1 = findViewById(R.id.button2);
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v) {
+            public void onClick(View arg0) {
+                Location gpsLocation;
+                gpsLocation = appLocationService.getLocation(LocationManager.GPS_PROVIDER);
+                if (gpsLocation != null) {
+                    double latitude = gpsLocation.getLatitude();
+                    double longitude = gpsLocation.getLongitude();
+                    String result = "Latitude: " + gpsLocation.getLatitude() +
+                            " Longitude: " + gpsLocation.getLongitude();
+                    tvAddress.setText(result);
+                } else {
+                    showSettingsAlert();
+                }
+            }
+        });
+        Button btn2 = findViewById(R.id.button3);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
 
-        }});
-        Button btn2 = (Button) findViewById(R.id.button3);
-        btn2.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick (View v) {
+                Location location = appLocationService
+                        .getLocation(LocationManager.GPS_PROVIDER);
+
+                //you can hard-code the lat & long if you have issues with getting it
+                //remove the below if-condition and use the following couple of lines
+                //double latitude = 37.422005;
+                //double longitude = -122.084095
+
+                if (location != null) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    LocationAddress locationAddress = new LocationAddress();
+                    locationAddress.getAddressFromLocation(latitude, longitude,
+                            getApplicationContext(), new GeocoderHandler());
+                } else {
+                    showSettingsAlert();
+                }
 
 
-                }});
-        Button btn3 = (Button) findViewById(R.id.button4);
-        btn3.setOnClickListener(new View.OnClickListener(){
+            }
+        });
+
+        Button btn3 = findViewById(R.id.button4);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newIntent = new Intent(MainActivity.this, HistoryActivity.class);
+                MainActivity.this.startActivity(newIntent);
+            }
+        });
+
+    }
+
+
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(
+                MainActivity.this);
+        alertDialog.setTitle("SETTINGS");
+        alertDialog.setMessage("Enable Location Provider! Go to settings menu?");
+        alertDialog.setPositiveButton("Settings",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(
+                                Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        MainActivity.this.startActivity(intent);
+                    }
+                });
+        alertDialog.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        alertDialog.show();
+    }
+
+    private class GeocoderHandler extends Handler {
         @Override
-        public void onClick (View v) {
-            Intent newIntent = new Intent(MainActivity.this, HistoryActivity.class);
-            MainActivity.this.startActivity(newIntent);
-                    }});
-
-
+        public void handleMessage(Message message) {
+            String locationAddress;
+            switch (message.what) {
+                case 1:
+                    Bundle bundle = message.getData();
+                    locationAddress = bundle.getString("address");
+                    break;
+                default:
+                    locationAddress = null;
+            }
+            tvAddress.setText(locationAddress);
+        }
     }
 }
