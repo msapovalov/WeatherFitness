c0dd3f9659520c08f8df336b7f64910aba0f5943
itbstudent <17217335+itbstudent@users.noreply.github.com>
2017-12-03 23:40:51 +0000

    Update TabAll.java

--- app/src/main/java/com/ucd/user/weatherfitness/TabAll.java
+++ app/src/main/java/com/ucd/user/weatherfitness/TabAll.java
@@ -11,7 +11,9 @@ import android.widget.SimpleCursorAdapter;
 
 /**
  * Created by mihhail_shapovalov on 11/18/17.
- */
+ *//
+
+//Class for All activities fragment
 
 public class TabAll extends Fragment {
 
@@ -19,19 +21,21 @@ public class TabAll extends Fragment {
     DBAdapter myDb;
     @Nullable
     @Override
+    //we inflate the fragment with view and populate list of workouts
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_history_all, container, false);
         populateListView(view);
         return view;
 
     }
-
+    //methods to run on fragment creation
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         myDb = new DBAdapter(getActivity());
-        myDb.openread();
+        myDb.openread();//open read only as we only need to show records here
     }
-
+    
+    //method to populate list from SQLite and using item_layout, custom layout to show records in particular order and size
     private void populateListView(View view) {
 
         Cursor cursor = myDb.getAllRows();
