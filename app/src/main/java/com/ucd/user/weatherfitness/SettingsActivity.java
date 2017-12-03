34e00a3bad00e889b05c0e01ca05dfb4b5edb5ec
itbstudent <17217335+itbstudent@users.noreply.github.com>
2017-12-03 23:35:32 +0000

    Update SettingsActivity.java

--- app/src/main/java/com/ucd/user/weatherfitness/SettingsActivity.java
+++ app/src/main/java/com/ucd/user/weatherfitness/SettingsActivity.java
@@ -8,30 +8,33 @@ import android.widget.Toast;
 public class SettingsActivity extends AppCompatActivity {
 
     DBAdapter myDb;
+    //shared preferences to find the position of switch
     private SharedPreferences.OnSharedPreferenceChangeListener listener;
     {
         listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
             @Override
             public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
                 if (key.equals("switch_preference_deletedb")){
-                    if (prefs.getBoolean(key,true)){
+                    if (prefs.getBoolean(key,true)){//of switch is ON, delete all records from table 
                     myDb.deleteAll();
+                    //Toast for user stating that records been deleted
                     Toast.makeText(SettingsActivity.this, "All Records in Database deleted", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
         };
     }
-
+    //on activity create run following methods
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //open db connection
         myDb = new DBAdapter(this);
         myDb.open();
-
+        //show fragment
         getSupportFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        sharedPreferences.registerOnSharedPreferenceChangeListener(listener);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(listener);//listener for switch
 
     }
 
