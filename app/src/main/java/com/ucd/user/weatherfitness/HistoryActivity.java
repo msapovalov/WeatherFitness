6012c10409d0a503dc7a5f3513483ab7bdb25569
itbstudent <17217335+itbstudent@users.noreply.github.com>
2017-12-03 23:21:38 +0000

    Update HistoryActivity.java

--- app/src/main/java/com/ucd/user/weatherfitness/HistoryActivity.java
+++ app/src/main/java/com/ucd/user/weatherfitness/HistoryActivity.java
@@ -9,18 +9,19 @@ import android.util.Log;
 public class HistoryActivity extends AppCompatActivity {
 
     private static final String TAG = "HistoryActivity";
-
+    //methods to run on creating activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
         Log.d(TAG, "onCreate:Starting");
-
+        //viewpager adaptor to work with fragments
         ViewPager mViewPager = findViewById(R.id.container);
         setupViewPager(mViewPager);
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
     }
+    //setting up fragments
     private void setupViewPager(ViewPager viewPager){
         SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
         adapter.addFragment(new TabAll(), "All Activities");
